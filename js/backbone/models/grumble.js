b01e5f6357af4951a7c547b308faaba0e38996a0
Silvia Brown <siltastic@gmail.com>
2015-09-01 10:25:21 -0400

    Edited comments name.

--- js/backbone/models/grumble.js
+++ js/backbone/models/grumble.js
@@ -1,7 +1,15 @@
-App.Models.Grumble = Backbone.Model.extend({
-  // initialize: function(){
-  //   console.log("New grumble!")
-  // },
+// App.Models.Grumble = Backbone.Model.extend({
+//   // initialize: function(){
+//   //   console.log("New grumble!")
+//   // },
 
-  urlRoot: "http://localhost:3000/grumbles"
+//   urlRoot: "http://localhost:3000/grumbles"
+// });
+
+App.Models.Grumble = Backbone.Model.extend({
+  urlRoot: "http://localhost:3000/comments",
+  initialize: function(){
+    this.comments = new App.Collections.Comments();
+    this.comments.url = this.url() + "/comments";
+  }
 });
